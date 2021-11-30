5bb454ed689f281bb409b6b3166f8342cd0dcb5e
O. N <anonymous@example.com>
2021-11-30 15:00:20 +0100

    init commit

---
+++ _next/static/fKObnZllYdjc8xwQCUUlY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/caaf31fdf28cd920.css","static/chunks/pages/index-549ed9aadc356dd0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
